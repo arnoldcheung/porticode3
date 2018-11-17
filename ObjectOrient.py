c17adc31ec3de208b71e974fe6fe1b2aaf4a33b5
JayYanHonCheung <45121122+JayYanHonCheung@users.noreply.github.com>
2018-11-17 20:58:25 +0000

    Sprites

---
+++ ObjectOrient.py
@@ -0,0 +1,153 @@
+import pygame
+pygame.init()       #initialize pygame
+
+win = pygame.display.set_mode((640,359))   #create window of 500x500
+pygame.display.set_caption("Donation Game")       #name the window to "string"
+
+#Loading images
+bg = pygame.image.load("background.png")         #Background image
+charSprite = pygame.image.load("sprites.png")    #Sprites image
+buttonUp = pygame.image.load("buttonup.png")     #Buttonup image
+ScaledButUp = pygame.transform.scale(buttonUp, (75,75))
+buttonDown = pygame.image.load("buttondown.png") #Buttondown image
+ScaledButDown = pygame.transform.scale(buttonDown,(75,75))
+#Create array of walking direction sprites
+'''
+walkRight = []
+walkLeft = []
+walkUp = []
+walkDown = []
+for i in range(3):
+    walkRight.append(charSprite.subsurface(i*64,128,64,64))
+    walkLeft.append(charSprite.subsurface(i*64,64,64,64))
+    walkUp.append(charSprite.subsurface(i*64,192,64,64))
+    walkDown.append(charSprite.subsurface(i*64,0,64,64))
+'''
+
+#Ingame clock
+clock = pygame.time.Clock()
+
+class player(object):
+    def __init__(self, x, y, width, height):
+        #Geometry and physics
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        #Movespeed variables
+        self.vel = 5
+        self.left = False
+        self.right = False
+        self.up = False
+        self.down = False
+        self.walkCount = 0
+        #Character attributes
+        self.exp = 0
+        self.level = 1
+
+        self.walkRight = []
+        self.walkLeft = []
+        self.walkUp = []
+        self.walkDown = []
+        for i in range(3):
+            self.walkRight.append(charSprite.subsurface(i * 64, 128, 64, 64))
+            self.walkLeft.append(charSprite.subsurface(i * 64, 64, 64, 64))
+            self.walkUp.append(charSprite.subsurface(i * 64, 192, 64, 64))
+            self.walkDown.append(charSprite.subsurface(i * 64, 0, 64, 64))
+
+    def draw(self, win):
+        # Cycling walking sprites
+        if self.walkCount + 1 >= 4:
+            self.walkCount = 0
+        # Call walking sprites
+        if self.left:
+            win.blit(self.walkLeft[self.walkCount], (self.x, self.y))
+            self.walkCount += 1
+        elif self.right:
+            win.blit(self.walkRight[self.walkCount], (self.x, self.y))
+            self.walkCount += 1
+        elif self.up:
+            win.blit(self.walkUp[self.walkCount], (self.x, self.y))
+            self.walkCount += 1
+        elif self.down:
+            win.blit(self.walkDown[self.walkCount], (self.x, self.y))
+            self.walkCount += 1
+        else:
+            win.blit(charSprite, (self.x, self.y),
+                     (64, 0, 64, 64))  # Default sprit into window (image, (position), (starting crop, end crop)
+
+# Drawing character
+def redrawGameWindow():     #Function of character update
+    man.draw(win)
+
+    #Rookie test
+    #win.fill((0, 0, 0))  # fill the window with black so that character doesnt duplicate
+    #pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))  # Character as a rectangle (placed in window, colour of rectangle, position and size)
+
+    #Putting images in window
+    win.blit(bg,(0,0))  #Background
+    win.blit(ScaledButUp,(300,150)) #Buttonup
+
+    #Hold mouse down to click button
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        win.blit(ScaledButDown, (300, 150))
+
+    #Experience bar
+    pygame.draw.rect(win, (255,255,25), (man.x+9, man.y-11, 50, 10))      #draw rectangle, (in window, colour, position, size)
+    pygame.draw.rect(win, (179,149,0),  (man.x+9, man.y-11, 50, 10))
+
+    #Level counter
+    text = font.render('Lv. ' + str(man.level), 1, (0,0,0))
+    win.blit(text, (man.x+9,man.y-13))
+
+    pygame.display.update()  # Update game
+
+#Main loop
+man = player(50, 50, 64, 64)
+font = pygame.font.SysFont('Arial', 12, True)   #Define font type, size, bold, italics
+run = True
+while run:
+    #pygame.time.delay(100)  #ingame clock, in milliseconds
+    clock.tick(9)
+    #Checking for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()    #create list of keys, origin in top left corner
+
+    if keys [pygame.K_LEFT] or keys [pygame.K_a]:
+        man.x -= man.vel
+        man.left = True
+        man.right = False
+        man.up = False
+        man.down = False
+    elif keys [pygame.K_RIGHT] or keys [pygame.K_d]:
+        man.x += man.vel
+        man.left = False
+        man.right = True
+        man.up = False
+        man.down = False
+    elif keys[pygame.K_DOWN] or keys [pygame.K_s]:
+        man.y += man.vel
+        man.left = False
+        man.right = False
+        man.up = False
+        man.down = True
+    elif keys[pygame.K_UP] or keys [pygame.K_w]:
+        man.y -= man.vel
+        man.left = False
+        man.right = False
+        man.up = True
+        man.down = False
+    else:
+        man.right = False
+        man.left = False
+        man.up = False
+        man.down = False
+        man.walkCount = 0
+
+    redrawGameWindow()  #Call func for character update
+
+
+pygame.quit()
