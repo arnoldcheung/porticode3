87a8c64eb06a61e47c0a4c09ba0842ad8b6e1f77
arnoldcheung <zceeacn@ucl.ac.uk>
2018-11-17 22:30:33 +0000

    object oriented fix

--- ObjectOrient.py
+++ ObjectOrient.py
@@ -1,11 +1,12 @@
 import pygame
 pygame.init()       #initialize pygame
 
-win = pygame.display.set_mode((640,359))   #create window of 500x500
+win = pygame.display.set_mode((640, 359))   #create window of 500x500
 pygame.display.set_caption("Donation Game")       #name the window to "string"
 
 #Loading images
-bg = pygame.image.load("background.png")         #Background image
+bg = pygame.image.load("background2.png")         #Background image
+scaled_bg = pygame.transform.scale(bg, (640, 359))
 charSprite = pygame.image.load("sprites.png")    #Sprites image
 buttonUp = pygame.image.load("buttonup.png")     #Buttonup image
 ScaledButUp = pygame.transform.scale(buttonUp, (75,75))
@@ -27,7 +28,7 @@ for i in range(3):
 #Ingame clock
 clock = pygame.time.Clock()
 
-class player(object):
+class player():
     def __init__(self, x, y, width, height):
         #Geometry and physics
         self.x = x
@@ -73,42 +74,45 @@ class player(object):
             win.blit(self.walkDown[self.walkCount], (self.x, self.y))
             self.walkCount += 1
         else:
-            win.blit(charSprite, (self.x, self.y),
-                     (64, 0, 64, 64))  # Default sprit into window (image, (position), (starting crop, end crop)
+            win.blit(charSprite, (self.x, self.y), (64, 0, 64, 64))  # Default sprit into window (image, (position), (starting crop, end crop)
 
 # Drawing character
 def redrawGameWindow():     #Function of character update
-    man.draw(win)
 
     #Rookie test
     #win.fill((0, 0, 0))  # fill the window with black so that character doesnt duplicate
     #pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))  # Character as a rectangle (placed in window, colour of rectangle, position and size)
 
     #Putting images in window
-    win.blit(bg,(0,0))  #Background
-    win.blit(ScaledButUp,(300,150)) #Buttonup
+    win.blit(scaled_bg, (0, 0))  #Background
+    win.blit(ScaledButUp, (300, 150)) #Buttonup
+    pygame.draw.AAfilledRoundedRect(win, (10, 10, 100, 100), (255, 255, 255),  0.5)
+    man.draw(win)
 
     #Hold mouse down to click button
     if event.type == pygame.MOUSEBUTTONDOWN:
+        pygame.draw.rect(win, (0, 0, 0), (10, 10, 100, 100))
         win.blit(ScaledButDown, (300, 150))
 
     #Experience bar
-    pygame.draw.rect(win, (255,255,25), (man.x+9, man.y-11, 50, 10))      #draw rectangle, (in window, colour, position, size)
-    pygame.draw.rect(win, (179,149,0),  (man.x+9, man.y-11, 50, 10))
+    pygame.draw.rect(win, (255, 255, 25), (man.x+9, man.y-11, 50, 10))      #draw rectangle, (in window, colour, position, size)
+    pygame.draw.rect(win, (179, 149, 0),  (man.x+9, man.y-11, 50, 10))
 
     #Level counter
-    text = font.render('Lv. ' + str(man.level), 1, (0,0,0))
-    win.blit(text, (man.x+9,man.y-13))
+    text = font.render('Lv. ' + str(man.level), 1, (0, 0, 0))
+    win.blit(text, (man.x+9, man.y-13))
 
     pygame.display.update()  # Update game
 
-#Main loop
+# Main loop
+
+
 man = player(50, 50, 64, 64)
 font = pygame.font.SysFont('Arial', 12, True)   #Define font type, size, bold, italics
 run = True
 while run:
     #pygame.time.delay(100)  #ingame clock, in milliseconds
-    clock.tick(9)
+    clock.tick(10)
     #Checking for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -116,36 +120,40 @@ while run:
 
     keys = pygame.key.get_pressed()    #create list of keys, origin in top left corner
 
-    if keys [pygame.K_LEFT] or keys [pygame.K_a]:
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
         man.x -= man.vel
         man.left = True
         man.right = False
         man.up = False
         man.down = False
-    elif keys [pygame.K_RIGHT] or keys [pygame.K_d]:
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         man.x += man.vel
         man.left = False
         man.right = True
         man.up = False
         man.down = False
-    elif keys[pygame.K_DOWN] or keys [pygame.K_s]:
+    elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
         man.y += man.vel
         man.left = False
         man.right = False
         man.up = False
         man.down = True
-    elif keys[pygame.K_UP] or keys [pygame.K_w]:
+    elif keys[pygame.K_UP] or keys[pygame.K_w]:
         man.y -= man.vel
         man.left = False
         man.right = False
         man.up = True
         man.down = False
+
     else:
+        man.walkCount = 1
+        '''
         man.right = False
         man.left = False
         man.up = False
         man.down = False
-        man.walkCount = 0
+        '''
+
 
     redrawGameWindow()  #Call func for character update
 
